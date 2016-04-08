945e18fbaaf205de7df61fefd70f184f8e4ab546
Karim Hmaissi <Karim Hmaissi>
2016-04-08 10:18:48 +0100

    getting apps

--- leaderboard.js
+++ leaderboard.js
@@ -121,19 +121,85 @@ var first_names = [
     ],
     categories = ["Genius", "Geek", "Hipster", "Idler"];;
 
-  Meteor.startup(function () {
-    if (Players.find().count() < 100) {
-      // ten thousand docs
-      for (var i = 0; i < 10 * 1000; i++) {
-        console.log(i + ' doc indexed');
-        Players.insert({
-          name: Random.choice(first_names) + ' ' + Random.choice(last_names),
-          score: Math.floor(Random.fraction() * 1000 / Random.fraction() / 100),
-          category: Random.choice(categories)
-        });
-      }
+  var getAppsList = function (callback) {
+    var response = HTTP.get('http://api.steampowered.com/ISteamApps/GetAppList/v0002/').data;
+    callback(error, response);
+  };
 
-      console.log('done!');
+  var getAppDetails = function (appId, callback) {
+    var response = HTTP.get('http://store.steampowered.com/api/appdetails/?appids=' + appId).data;
+    if(response[appId].success + "" === "true") {
+      console.log('got successful result!!!!!!!');
+      callback(error, response[appId].data);
+    } else {
+      console.log("got false from success");
+      return false;
+    } 
+  };
+
+  var processAppsList = function (err, response) {
+   if(response) {
+     console.log('got a response');
+
+     var apps = response.applist.apps;
+     console.log(apps.length);
+
+     var i = 0, length = apps.length;
+
+     for(i; i < length; i++) {
+       Apps.insert(apps[i]);
+       console.log(apps[i].name + " saved");
+     }
+   }
+  });
+
+
+  var getDetails = function () {
+    var appIds = Apps.find().fetch();
+    var i = 0; length = appIds.length; 
+
+    var callback = function (error, result) {
+      if(result) {
+        console.log('got successful response');
+        console.log(result.type);
+        console.log(result.name);
+        console.log(result.steam_appid);
+        AppDetails.insert(result);
+      } else {
+        console.log('result was false');
+      }
+    });
+
+
+    i++;
+    if(i >= length) {
+      clearInterval(interval);
     }
+
+    var interval = Meteor.setInterval(function () {
+      var appId = appIds[i].appid;
+
+      getAppDetails(appId, callback);
+
+    }, 5000)
+  };
+
+
+  Meteor.startup(function () {
+
+    getAppsList(processAppsList);
+
+    // if (Players.find().count() < 100) {
+    //   for (var i = 0; i < 10 * 1000; i++) {
+    //     console.log(i + ' doc indexed');
+    //     Players.insert({
+    //       name: Random.choice(first_names) + ' ' + Random.choice(last_names),
+    //       score: Math.floor(Random.fraction() * 1000 / Random.fraction() / 100),
+    //       category: Random.choice(categories)
+    //     });
+    //   }
+
+    //   console.log('done!');
+    // }
   });
 }
